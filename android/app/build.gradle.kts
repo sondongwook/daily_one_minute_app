75ef169862b7b48cbdc3f3df7b97c3fc55a14fa1
Son dong-wook <care20@naver.com>
2025-05-12 23:57:38 +0900

    서명키 등록

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +7,12 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties().apply {
+    load(File(rootDir, "key.properties").inputStream())
+}
+
 android {
-    namespace = "com.example.daily_one_minute_app"
+    namespace = "com.appdev1016.daily_one_minute_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -24,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.daily_one_minute_app"
+        applicationId = "com.appdev1016.daily_one_minute_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -33,11 +39,22 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = File(rootDir, keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/appdev1016/daily_one_minute_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/appdev1016/daily_one_minute_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.daily_one_minute_app
+package com.appdev1016.daily_one_minute_app
 
 import io.flutter.embedding.android.FlutterActivity
 
